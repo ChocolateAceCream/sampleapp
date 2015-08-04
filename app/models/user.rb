e481029759331472c905d4801306f1d5b26333a1
ChocolateAceCream <nuodi@hotmail.com>
2015-08-04 10:39:54 -0400

    log in finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,15 @@ class User < ActiveRecord::Base
     before_save{self.email=email.downcase}
     validates(:name, presence: true, length: {maximum: 50})
     validates :email, presence: true, length:{maximum: 255},
-            format:{ with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},
-            uniqueness: {case_sensitive: false}
+        format:{ with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},
+        uniqueness: {case_sensitive: false}
     has_secure_password
     validates :password, presence: true, length: {minimum: 6}
+
+    # Returns the hash digest of the given string
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+            BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
 end
