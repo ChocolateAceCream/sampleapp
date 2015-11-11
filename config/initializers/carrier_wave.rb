174e955d45efdc8259777ba571316203868cb4f4
ChocolateAceCream <nuodi@hotmail.com>
2015-11-11 17:45:11 -0500

    fix amazon #2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,5 @@
 if Rails.env.production?
- config.root = Rails.root.join('tmp') # adding these...
-  config.cache_dir = 'carrierwave' # ...two lines
+  CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
@@ -8,4 +7,5 @@ if Rails.env.production?
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
+  end
 end
